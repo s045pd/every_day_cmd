80debf06cff0db21cd9703b27665c9a317c673fd
ai3 <aoii103@126.com>
2019-10-15 17:26:26 +0800

    add conf.py

---
+++ conf.py
@@ -0,0 +1,11 @@
+class conf:
+    ip = '0.0.0.0'
+    port = 8000
+    
+    
+    root_git_path = "./tldr"
+    root_file = "./tldr/pages"
+    root_git_url = "https://github.com/tldr-pages/tldr.git"
+
+
+config = conf
