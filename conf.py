11a8d9df795aa932b75d81fd902228cac2481d6c
ai3 <aoii103@126.com>
2019-10-16 14:32:51 +0800

    update conf

--- conf.py
+++ conf.py
@@ -7,12 +7,14 @@ class conf:
     root_git_url = "https://github.com/tldr-pages/tldr.git"
 
     types_by_ua = ["osx", "window", "sunos", "linux", "common"]
-    types_by_user = {"osx": "osx",
-                     "window": "window",
-                     "sunos": "sunos",
-                     "linux": "linux",
-                     "common": "common",
-                     "all":"**"}
+    types_by_user = {
+        "osx": "osx",
+        "window": "window",
+        "sunos": "sunos",
+        "linux": "linux",
+        "common": "common",
+        "all": "**",
+    }
 
 
 config = conf
